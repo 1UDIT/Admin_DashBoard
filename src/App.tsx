18aa19f18cd6c6b1a92bc190fa90f28edf11df78
u121 <uditsharma7195@gmail.com>
2024-03-06 19:34:03 +0530

    dark mode not fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,6 @@ import Sidebar from './components/sliderBar'
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    // <main className='bg-black h-dvh flex-col items-center w-screen justify-between'>
-    //   <Navbar/>
-    //   <CardScreen/> 
-    // </main >
-    // <div className="bg-black  grid-cols-2 grid-flow-col md:grid-cols-2 md:h-dvh " >
-    //   <div className="row-span-1 w-1/2">
-    //     <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-    //   </div>
-    //   <div className="bg-black  grid-rows-1 grid-flow-col md:grid-rows-2 md:h-dvh " >
-    //     <div className="row-span-1 h-16"><Navbar /></div>
-    //     <div className="col-span-1 p-4 gap-4 "><CardScreen /> </div>
-    //   </div>
-    // </div>
     <div className="dark:bg-boxdark-2 dark:text-bodydark">
       {/* <!-- ===== Page Wrapper Start ===== --> */}
       <div className="flex h-screen overflow-hidden">
@@ -28,14 +15,14 @@ function App() {
         {/* <!-- ===== Sidebar End ===== --> */}
 
         {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden bg-black">
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           {/* <!-- ===== Header Start ===== --> */}
           <Navbar />
           {/* <!-- ===== Header End ===== --> */}
 
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 bg-black">
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               <CardScreen />
             </div>
           </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import DarkModeSwitcher from './darkModeSwitcher'
 
 export default function Navbar() {
     return (
         <div className='bg-[#1f2937] h-12'>
             <div className='p-3 text-white'>
                 <h4>DashBord</h4>
+                <DarkModeSwitcher/>
             </div>
         </div>
     )
