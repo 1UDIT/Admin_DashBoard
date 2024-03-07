5706e266c46e46a6c7d3962efa804803ca6cf3df
u121 <uditsharma7195@gmail.com>
2024-03-07 18:05:17 +0530

    darkSwitcher FIx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import DarkModeSwitcher from './darkModeSwitcher'
 
 export default function Navbar() {
     return (
-        <div className='bg-[#1f2937] h-12'>
-            <div className='p-3 text-white'>
-                <h4>DashBord</h4>
-                <DarkModeSwitcher/>
+        <header className='sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none'>
+            <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+                <div className="col-span-1 w-80"> <h4>DashBord</h4></div>
+                <div className="flex items-center gap-3 2xsm:gap-7"><DarkModeSwitcher /></div>
             </div>
-        </div>
+        </header>
     )
 }
